4a8766e66caa958a73ded916745471285e74e09b
Ethan Schofer <ethan.schofer@gmail.com>
2015-10-03 16:38:41 -0400

    Add foundation

--- AdminDashboardWithFoundation.Web/App_Start/BundleConfig.cs
+++ AdminDashboardWithFoundation.Web/App_Start/BundleConfig.cs
@@ -1,4 +1,4 @@
-﻿using System.Web;
+using System.Web;
 using System.Web.Optimization;
 
 namespace AdminDashboardWithFoundation.Web
@@ -19,13 +19,14 @@ namespace AdminDashboardWithFoundation.Web
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
-
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                "~/Content/simple-sidebar.css",
+           "~/Content/site.css"));
+
+            #region Foundation Bundles
+
+            bundles.Add(Foundation.Scripts());
+            #endregion
         }
     }
 }
